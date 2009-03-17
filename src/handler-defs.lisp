418ff8483ad1204b61ad7bb9c98ec9964804b755
Chaitanya Gupta <mail@chaitanyagupta.com>
2009-03-17 15:53:58 +0530

    Changed syntax for DEFINE-HANDLER.

--- src/handler-defs.lisp
+++ src/handler-defs.lisp
@@ -2,6 +2,100 @@
 
 (clear-handlers)
 
+;;; Date handlers
+
+(defun merge-time-tokens-day (tokens date-start)
+  (let ((time (awhen tokens
+                (let ((*now* date-start))
+                  (tokens-to-span it)))))
+    (if time
+        (make-span (merge-datetime date-start (span-start time))
+                   (merge-datetime date-start (span-end time)))
+        (make-span date-start (datetime-incr date-start :day)))))
+
+(define-handler (date handle-rmn-sd-sy)
+    ((repeater-month-name scalar-day scalar-year)
+     (repeater-month-name scalar-day scalar-year (? separator-at) (? p time)))
+    (tokens)
+  (setf tokens (remove-separators tokens))
+  (let* ((year-tag (find-tag 'scalar-year (third tokens)))
+         (month-name-tag (find-tag 'repeater-month-name (first tokens)))
+         (day-tag (find-tag 'scalar-day (second tokens)))
+         (date-start (make-date (tag-type year-tag)
+                                (month-index (tag-type month-name-tag))
+                                (tag-type day-tag))))
+    (merge-time-tokens-day (nthcdr 3 tokens) date-start)))
+
+(define-handler (date handle-rmn-sd)
+    ((repeater-month-name scalar-day (? separator-at) (? p time)))
+    (tokens)
+  (setf tokens (remove-separators tokens))
+  (let* ((month-name-tag (find-tag 'repeater-month-name (first tokens)))
+         (day-tag (find-tag 'scalar-day (second tokens)))
+         (month (month-index (tag-type month-name-tag)))
+         (day (tag-type day-tag))
+         (this-year (year-of *now*))
+         (this-year-date (make-date this-year month day))
+         (year (ecase *context*
+                 (:future (if (datetime< this-year-date *now*) (1+ this-year) this-year))
+                 (:past (if (datetime> this-year-date *now*) (1- this-year) this-year))))
+         (date-start (make-date year month day)))
+    (merge-time-tokens-day (nthcdr 2 tokens) date-start)))
+
+(define-handler (date)
+    ((repeater-time (? repeater-day-portion) (? separator-on) repeater-month-name scalar-day))
+    (tokens)
+  (setf tokens (remove-separators tokens))
+  (cond
+    ((= (length tokens) 3)
+     (handle-rmn-sd (list (second tokens) (third tokens) (first tokens))))
+    ((= (length tokens) 4)
+     (handle-rmn-sd (list (third tokens) (fourth tokens) (first tokens) (second tokens))))
+    (t
+     (error "Wrong number of tokens passed to HANDLE-RMN-SD-ON.~%Tokens:~%~S" tokens))))
+
+(define-handler (date handle-rmn-od)
+    ((repeater-month-name ordinal-day (? separator-at) (? p time)))
+    (tokens)
+  (let* ((day-token (second tokens))
+         (day (tag-type (find-tag 'ordinal-day day-token))))
+    (tag (create-tag 'scalar-day day) day-token)
+    (handle-rmn-sd (list* (first tokens) day-token (nthcdr 2 tokens)))))
+
+(define-handler (date)
+    ((repeater-time (? repeater-day-portion) (? separator-on) repeater-month-name ordinal-day))
+    (tokens)
+  (setf tokens (remove-separators tokens))
+  (cond
+    ((= (length tokens) 3)
+     (handle-rmn-od (list (second tokens) (third tokens) (first tokens))))
+    ((= (length tokens) 4)
+     (handle-rmn-od (list (third tokens) (fourth tokens) (first tokens) (second tokens))))
+    (t
+     (error "Wrong number of tokens passed to HANDLE-RMN-OD-ON.~%Tokens:~%~S" tokens))))
+
+(define-handler (date)
+    ((repeater-month-name scalar-year))
+    (tokens)
+  (let* ((month-name (tag-type (find-tag 'repeater-month-name (first tokens))))
+         (month (month-index month-name))
+         (year (tag-type (find-tag 'scalar-year (second tokens))))
+         (start (make-date year month)))
+    (make-span start (datetime-incr start :month))))
+
+(define-handler (date)
+    ((scalar-day repeater-month-name scalar-year (? separator-at) (? p time)))
+    (tokens)
+  (handle-rmn-sd-sy (list* (second tokens) (first tokens) (nthcdr 2 tokens))))
+
+(define-handler (date)
+    ((ordinal-day repeater-month-name scalar-year (? separator-at) (? p time)))
+    (tokens)
+  (let* ((day-token (first tokens))
+         (day (tag-type (find-tag 'ordinal-day day-token))))
+    (tag (create-tag 'scalar-day day) day-token)
+    (handle-rmn-sd-sy (list* (second tokens) day-token (nthcdr 2 tokens)))))
+
 ;;; Time handlers
 
 (defun dealias-time (time &optional day-portion &aux
@@ -22,100 +116,8 @@
              (datetime-incf (tick-time tick) :hour 12)))))
     (r-next repeater-time *context*)))
 
-(define-handler (time-rt-rdp
-                 (repeater-time (? repeater-day-portion)))
+(define-handler (time)
+    ((repeater-time (? repeater-day-portion)))
     (tokens)
   (apply #'dealias-time tokens))
-
-;;; Date handlers
-
-(defun merge-time-tokens-day (tokens date-start)
-  (let ((time (awhen tokens
-                (let ((*now* date-start))
-                  (tokens-to-span it)))))
-    (if time
-        (make-span (merge-datetime date-start (span-start time))
-                   (merge-datetime date-start (span-end time)))
-        (make-span date-start (datetime-incr date-start :day)))))
-
-(define-handler (date-rmn-sd-sy
-                 (repeater-month-name scalar-day scalar-year)
-                 (repeater-month-name scalar-day scalar-year (? separator-at) (? p time)))
-    (tokens)
-  (setf tokens (remove-separators tokens))
-  (let* ((year-tag (find-tag 'scalar-year (third tokens)))
-         (month-name-tag (find-tag 'repeater-month-name (first tokens)))
-         (day-tag (find-tag 'scalar-day (second tokens)))
-         (date-start (make-date (tag-type year-tag)
-                                (month-index (tag-type month-name-tag))
-                                (tag-type day-tag))))
-    (merge-time-tokens-day (nthcdr 3 tokens) date-start)))
-
-(define-handler (date-rmn-sd
-                 (repeater-month-name scalar-day (? separator-at) (? p time)))
-    (tokens)
-  (setf tokens (remove-separators tokens))
-  (let* ((month-name-tag (find-tag 'repeater-month-name (first tokens)))
-         (day-tag (find-tag 'scalar-day (second tokens)))
-         (month (month-index (tag-type month-name-tag)))
-         (day (tag-type day-tag))
-         (this-year (year-of *now*))
-         (this-year-date (make-date this-year month day))
-         (year (ecase *context*
-                 (:future (if (datetime< this-year-date *now*) (1+ this-year) this-year))
-                 (:past (if (datetime> this-year-date *now*) (1- this-year) this-year))))
-         (date-start (make-date year month day)))
-    (merge-time-tokens-day (nthcdr 2 tokens) date-start)))
-
-(define-handler (date-rmn-sd-on
-                 (repeater-time (? repeater-day-portion) (? separator-on) repeater-month-name scalar-day))
-    (tokens)
-  (setf tokens (remove-separators tokens))
-  (cond
-    ((= (length tokens) 3)
-     (date-rmn-sd (list (second tokens) (third tokens) (first tokens))))
-    ((= (length tokens) 4)
-     (date-rmn-sd (list (third tokens) (fourth tokens) (first tokens) (second tokens))))
-    (t
-     (error "Wrong number of tokens passed to DATE-RMN-SD-ON.~%Tokens:~%~S" tokens))))
-
-(define-handler (date-rmn-od
-                 (repeater-month-name ordinal-day (? separator-at) (? p time)))
-    (tokens)
-  (let* ((day-token (second tokens))
-         (day (tag-type (find-tag 'ordinal-day (second tokens)))))
-    (tag (create-tag 'scalar-day day) day-token)
-    (date-rmn-sd (list* (first tokens) day-token (nthcdr 2 tokens)))))
-
-(define-handler (date-rmn-od-on
-                 (repeater-time (? repeater-day-portion) (? separator-on) repeater-month-name ordinal-day))
-    (tokens)
-  (setf tokens (remove-separators tokens))
-  (cond
-    ((= (length tokens) 3)
-     (date-rmn-od (list (second tokens) (third tokens) (first tokens))))
-    ((= (length tokens) 4)
-     (date-rmn-od (list (third tokens) (fourth tokens) (first tokens) (second tokens))))
-    (t
-     (error "Wrong number of tokens passed to DATE-RMN-OD-ON.~%Tokens:~%~S" tokens))))
-
-(define-handler (date-rmn-sy
-                 (repeater-month-name scalar-year))
-    (tokens)
-  (let* ((month-name (tag-type (find-tag 'repeater-month-name (first tokens))))
-         (month (month-index month-name))
-         (year (tag-type (find-tag 'scalar-year (second tokens))))
-         (start (make-date year month)))
-    (make-span start (datetime-incr start :month))))
-
-(define-handler (date-sd-rmn-sy
-                 (scalar-day repeater-month-name scalar-year (? separator-at) (? p time)))
-    (tokens)
-  (date-rmn-sd-sy (list* (second tokens) (first tokens) (nthcdr 2 tokens))))
-
-
-
-
-
-
 
--- src/handlers.lisp
+++ src/handlers.lisp
@@ -1,44 +1,52 @@
 (cl:in-package #:chronicity)
 
-(defparameter *handlers* (make-hash-table))
 (defvar *endian-handler-1* nil)
 (defvar *endian-handler-2* nil)
 
-(defun make-handler (pattern fn)
-  (cons pattern fn))
+(defun make-handler (class pattern fn)
+  (list class pattern fn))
+
+(defun handler-class (handler)
+  (first handler))
 
 (defun handler-pattern (handler)
-  (car handler))
+  (second handler))
 
 (defun handler-fn (handler)
-  (cdr handler))
+  (third handler))
+
+(defparameter *handlers* nil)
 
 (defun clear-handlers ()
-  (clrhash *handlers*))
+  (setf *handlers* nil))
 
 (defun find-class-handlers (class)
-  (gethash class *handlers*))
+  (remove class *handlers* :key #'handler-class :test (complement #'eql)))
 
-(defmacro define-handler ((name &rest patterns) &optional lambda-list &body body)
-  (let ((class (intern (first (cl-ppcre:split "-" (string name))) :chronicity)))
-    `(progn
-       , (when (find #\- (string name))
-           `(defun ,name ,lambda-list
-              ,@body))
-       (add-handler ',class ',patterns ',name))))
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defun make-handler-name ()
+    (intern (format nil "HANDLE-~A" (incf *gensym-counter*)) :chronicity)))
+
+(defmacro define-handler ((class &optional (name (make-handler-name)))
+                          patterns &optional lambda-list
+                          &body body)
+  `(progn
+     , (when (and lambda-list body)
+         `(defun ,name ,lambda-list
+            ,@body))
+     (add-handler ',class ',patterns ',name)))
 
 (defun add-handler (class patterns fn)
   (let ((handlers (loop
                      for pattern in patterns
-                     collect (make-handler pattern fn))))
-    (setf (gethash class *handlers*)
-          (nconc (gethash class *handlers*) handlers))))
+                     collect (make-handler class pattern fn))))
+    (setf *handlers* (nconc *handlers* handlers))))
 
-(setf (gethash 'time *handlers*)
-      (list (make-handler '(repeater-time (? repeater-day-portion)) nil))) ; DONE
+(progn
+      (list (make-handler 'time '(repeater-time (? repeater-day-portion)) nil))) ; DONE
 
-(setf (gethash 'date *handlers*)
-      (list (make-handler '(repeater-day-name repeater-month-name scalar-day repeater-time (? separator-slash-or-dash) time-zone scalar-year) 'handle-rdn-rmn-sd-t-tz-sy)
+(progn
+      (list (make-handler 'date '(repeater-day-name repeater-month-name scalar-day repeater-time (? separator-slash-or-dash) time-zone scalar-year) 'handle-rdn-rmn-sd-t-tz-sy)
             ; (make-handler '(repeater-month-name scalar-day scalar-year) 'handle-rmn-sd-sy)
             ; (make-handler '(repeater-month-name scalar-day scalar-year (? separator-at) (? p time)) 'handle-rmn-sd-sy)
             ; (make-handler '(repeater-month-name scalar-day (? separator-at) (? p time)) 'handle-rmn-sd)
@@ -49,42 +57,48 @@
             ; (make-handler '(scalar-day repeater-month-name scalar-year (? separator-at) (? p time)) 'handle-sd-rmn-sy)
             *endian-handler-1*
             *endian-handler-2*
-            (make-handler '(scalar-year separator-slash-or-dash scalar-month separator-slash-or-dash scalar-day (? separator-at) (? p 'time)) 'handle-sy-sm-sd)
-            (make-handler '(scalar-day separator-slash-or-dash scalar-month) 'handle-sd-sm)
-            (make-handler '(scalar-month separator-slash-or-dash scalar-year) 'handle-sm-sy)))
+            (make-handler 'date '(scalar-year separator-slash-or-dash scalar-month separator-slash-or-dash scalar-day (? separator-at) (? p 'time)) 'handle-sy-sm-sd)
+            (make-handler 'date '(scalar-day separator-slash-or-dash scalar-month) 'handle-sd-sm)
+            (make-handler 'date '(scalar-month separator-slash-or-dash scalar-year) 'handle-sm-sy)))
 
-(setf (gethash 'anchor *handlers*)
-      (list (make-handler '((? grabber) repeater (? separator-at) (? repeater) (? repeater)) 'handle-r)
-            (make-handler '((? grabber) repeater repeater (? separator-at) (? repeater) (? repeater)) 'handle-r)
-            (make-handler '(repeater grabber repeater) 'handle-r-g-r)))
+(progn
+      (list (make-handler 'anchor '((? grabber) repeater (? separator-at) (? repeater) (? repeater)) 'handle-r)
+            (make-handler 'anchor '((? grabber) repeater repeater (? separator-at) (? repeater) (? repeater)) 'handle-r)
+            (make-handler 'anchor '(repeater grabber repeater) 'handle-r-g-r)))
 
-(setf (gethash 'arrow *handlers*)
-      (list (make-handler '(scalar repeater pointer) 'handle-s-r-p)
-            (make-handler '(pointer scalar repeater) 'handle-p-s-r)
-            (make-handler '(scalar repeater pointer (? p 'anchor)) 'handle-s-r-p-a)))
+(progn
+      (list (make-handler 'arrow '(scalar repeater pointer) 'handle-s-r-p)
+            (make-handler 'arrow '(pointer scalar repeater) 'handle-p-s-r)
+            (make-handler 'arrow '(scalar repeater pointer (? p 'anchor)) 'handle-s-r-p-a)))
 
-(setf (gethash 'narrow *handlers*)
-      (list (make-handler '(ordinal repeater separator-in repeater) 'handle-o-r-s-r)
-            (make-handler '(ordinal repeater grabber repeater) 'handle-o-r-g-r)))
+(progn
+      (list (make-handler 'narrow '(ordinal repeater separator-in repeater) 'handle-o-r-s-r)
+            (make-handler 'narrow '(ordinal repeater grabber repeater) 'handle-o-r-g-r)))
 
 ;;; TODO: Remove the unwanted tokens
+
+(defvar *handler* nil
+  "Current handler -- The one which matched the token.")
+
 (defun match-tokens (handler tokens)
   (let ((tokens* tokens)
+        (token-index 0)
         (pattern (handler-pattern handler)))
     (flet ((!next-token ()
+             (incf token-index)
              (setf tokens* (cdr tokens*))
              (car tokens*))
            (!optionalp (element)
              (unless (atom element)
                (member '? element))))
-      (loop named loop
+      (loop
          for pattern* on pattern
          for element = (car pattern*)
          for token = (or (first tokens*)
                          (if (every #'!optionalp pattern*)
-                             (return-from loop nil)
-                             (return-from match-tokens nil)))
-         for name = (if (atom element) element (second element))
+                             (return t)
+                             (return nil)))
+         for name = (if (atom element) element (first (last element)))
          for optionalp = (!optionalp element)
          for sub-handler-p = (unless (atom element)
                                (member 'p element))
@@ -93,16 +107,22 @@
          if (not sub-handler-p)
          do (if (token-has-tag-p token name)
                 (!next-token)
-                (unless optionalp (return-from match-tokens nil)))
+                (unless optionalp (return nil)))
          else do (let ((sub-handlers (find-class-handlers name)))
-                   (loop
+                   (loop named inner-loop
                       for sub-handler in sub-handlers
-                      thereis (match-tokens sub-handler tokens*)
-                      finally (unless optionalp (return-from match-tokens nil))))))
-    (handler-fn handler)))
+                      thereis (awhen (match-tokens sub-handler tokens*)
+                                (loop
+                                   repeat it
+                                   do (!next-token))
+                                t)
+                      finally (unless optionalp (return nil))))
+         finally (if tokens*
+                     (return nil)
+                     (return token-index))))))
 
 ;;; TODO: Do this!
-(defun tokens-to-span (tokens)
+#|(defun tokens-to-span (tokens)
   (flet ((!match (class)
            (let ((handlers (find-class-handlers class)))
              (dolist (handler handlers)
@@ -112,7 +132,13 @@
     (!match 'anchor)
     (!match 'arrow)
     (!match 'marrow)
-    (!match 'time)))
+    (!match 'time)))|#
+
+(defun tokens-to-span (tokens)
+  (dolist (handler *handlers* nil)
+    (when (match-tokens handler tokens)
+      (let ((*handler* handler))
+        (return-from tokens-to-span (funcall (handler-fn handler) tokens))))))
 
 (defun remove-separators (tokens)
   (remove-if #'(lambda (token)
